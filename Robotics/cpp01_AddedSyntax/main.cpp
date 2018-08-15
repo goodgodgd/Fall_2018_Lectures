152a618415bb427e818ad97a673a77b0658a13b9
goodgodgd <goodgodgd@yonsei.ac.kr>
2018-08-15 14:58:28 +0900

    add project cpp basic syntax

---
+++ Robotics/cpp01_AddedSyntax/main.cpp
@@ -0,0 +1,27 @@
+#include "cpp_functions.h"
+
+int main()
+{
+    std::cout << "Hello Students!" << std::endl;
+
+    declareVariableAnywhere();
+
+    useBoolType();
+
+    useStructWithoutStruct();
+
+    casting();
+
+    newAndDeleteSimple();
+    newAndDelete1DArray();
+    newAndDelete2DArray();
+
+    callByReference();
+
+    defaultArgument();
+
+    functionOverloading();
+    functionOverloading(1, 3.1f);
+
+    return 0;
+}
