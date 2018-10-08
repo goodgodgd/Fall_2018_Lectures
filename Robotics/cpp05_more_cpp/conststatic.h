bf87a53182e2ae2b1497092aace817f7b4559bf8
goodgodgd <goodgodgd@yonsei.ac.kr>
2018-10-08 10:13:27 +0900

    update const static

--- Robotics/cpp05_more_cpp/conststatic.h
+++ Robotics/cpp05_more_cpp/conststatic.h
@@ -22,15 +22,8 @@ public:
     Student(int _score)
     {
         score = _score;
-        // static 멤버 함수/변수는 일반적인 함수에서 접근가능하다.
+        // static 멤버 함수/변수는 일반적인 멤버 함수에서 접근가능하다.
         cout << ++n_students << "'th Student is created." << endl;
-    }
-
-    static void numberOfStudents()
-    {
-        // static 멤버 함수에서는 static 멤버 변수만 접근할 수 있다.
-        cout << "Number of students: " << n_students << endl;
-        // static 함수에서는 일반 멤버 함수/변수에 접근할 수 없다.
     }
 
     void study()
@@ -38,13 +31,24 @@ public:
         score++;
     }
 
+    void setScore(const int _score)
+    {
+        score = _score;
+    }
+
     // 클래스 객체를 const로 선언한 경우 멤버 함수를 사용할 수 없다.
     // 하지만 내부 속성을 건드리지 않는 함수는 함수 선언 끝에 const를 붙이면
     // const 객체에서도 호출할 수 있다.
     int getScore() const
     {
-        cout << "my score is " << score << endl;
         return score;
+    }
+
+    static void numberOfStudents()
+    {
+        // static 멤버 함수에서는 static 멤버 변수만 접근할 수 있다.
+        cout << "Number of students: " << n_students << endl;
+        // static 함수에서는 일반 멤버 함수/변수에 접근할 수 없다.
     }
 
 private:
@@ -85,11 +89,18 @@ void practiceConstStatic()
     Student kim(78);
     Student* lee = new Student(90);
 
-    // 일반 함수는 객체를 통해서만 호출가능하다.
-    kim.getScore();
-    lee->getScore();
+    // 일반 함수는 객체를 통해서만 호출 가능하다.
+    cout << "kim's score is " << kim.getScore() << endl;
+    kim.setScore(85);
+    cout << "kim's score is updated to " << kim.getScore() << endl;
+    cout << "lee's score is " << lee->getScore() << endl;
 
-    // static 멤버 함수는 클래스에서 직접 부를수도 있고 객체에서 부를수도 있다.
+    // const 객체는 const 함수만 부를 수 있다.
+    const Student park(60);
+    cout << "const park's score: " << park.getScore() << endl;
+//    cout << "const park's score: " << park.setScore(50) << endl;
+
+    // static 멤버 함수는 클래스에서 직접 부를 수도 있고 객체에서 부를수도 있다.
     Student::numberOfStudents();
     kim.numberOfStudents();
     lee->numberOfStudents();
