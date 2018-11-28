5d417d534b7630d242b87d9c047df9111737d390
goodgodgd <goodgodgd@yonsei.ac.kr>
2018-11-28 14:32:36 +0900

    add example 6-10

---
+++ ControlEng/example_6_10.m
@@ -0,0 +1,54 @@
+clc; clear
+
+s = tf('s');
+G1 = 1/(s*(s+4)*(s+6));
+
+disp('root locus 그리기')
+figure(1)
+rlocus(G1)
+axis([-12 2 -7 7])
+set(gcf,'Position',[300 300 600 600])
+hold on
+
+disp('Mp로부터 zeta 계산')
+Mp = 0.16;
+v1 = log(Mp);
+zeta_Mp = sqrt(v1^2/(pi^2+v1^2))
+disp('조건을 만족하는 zeta를 선택')
+zeta = 0.504
+theta = asin(zeta);
+plot([0 -100*sin(theta)], [0  100*cos(theta)], 'c--')
+plot([0 -100*sin(theta)], [0 -100*cos(theta)], 'c--')
+
+disp('근궤적에서 Mp 조건을 만족하는 극점 선택')
+p1 = [-1.205, 2.064]
+plot(p1(1), p1(2), '*k')
+
+disp('상승시간을 1/3로 줄이는 극점 설계')
+p2 = p1*3
+plot(p2(1), p2(2), '*r')
+hold off
+
+
+disp('위상조건에 의한 영점의 위상 계산')
+ang_G1 = -atan2(p2(2), p2(1)) - atan2(p2(2), p2(1)+4) ...
+            - atan2(p2(2), p2(1)+6);
+ang_G1 = ang_G1;
+% theta_z + ang_G1 = -180
+theta_z = -pi - ang_G1;
+theta_z_deg = rad2deg(theta_z)
+zero = p2(2)/tan(theta_z) - p2(1)
+
+G2 = (s+zero)/(s*(s+4)*(s+6));
+disp('root locus 그리기')
+figure(2)
+rlocus(G2)
+axis([-12 2 -7 7])
+set(gcf,'Position',[900 300 600 600])
+hold on
+plot(p2(1), p2(2), '*r')
+hold off
+
+K = sqrt(p2(1)^2 + p2(2)^2) * sqrt((4+p2(1))^2 + p2(2)^2) ...
+    * sqrt((6+p2(1))^2 + p2(2)^2) / sqrt((zero+p2(1))^2 + p2(2)^2)
+
--- ControlEng/example_6_11.m
+++ ControlEng/example_6_11.m
@@ -3,15 +3,34 @@ clc; clear
 zeta = 0.456;
 izeta = sqrt(1-zeta^2);
 wn = 11.87;
+x1 = -zeta*wn; y1 = izeta*wn;
 
-x = -2*zeta*wn
-y = 2*izeta*wn
+s = tf('s');
+G1 = (s+8)/((s+3)*(s+6)*(s+10));
+figure(1)
+plot(x1, y1, '*k')
+hold on
+rlocus(G1)
+axis([-50 2 -25 25])
+set(gcf,'Position',[100 100 600 600])
+hold off
 
-z1 = rad2deg(atan2(y,8+x))
-p1 = rad2deg(atan2(y,3+x))
-p2 = rad2deg(atan2(y,6+x))
-p3 = rad2deg(atan2(y,10+x))
+x2 = 2*x1; y2 = 2*y1;
+z1 = rad2deg(atan2(y2,8+x2))
+p1 = rad2deg(atan2(y2,3+x2))
+p2 = rad2deg(atan2(y2,6+x2))
+p3 = rad2deg(atan2(y2,10+x2))
 
 ang_G = z1-p1-p2-p3
 ang_zc = deg2rad(-180 - ang_G)
-zc = y/tan(ang_zc)-x
+zc = y2/tan(ang_zc)-x2
+
+s = tf('s');
+G2 = (s+zc)*(s+0.6)*(s+8)/(s*(s+3)*(s+6)*(s+10));
+figure(2)
+plot(x2, y2, '*k')
+hold on
+rlocus(G2)
+axis([-100 2 -50 50])
+set(gcf,'Position',[800 100 600 600])
+hold off
